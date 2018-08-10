cc15a17ab7dbfd10c33c23314987016e12045737
Colin Binion <colin.binion@me.com>
2018-08-09 20:44:47 -0600

    something

---
+++ redo/dangDingo/config/session.js
@@ -0,0 +1,39 @@
+/**
+ * Session Configuration
+ * (sails.config.session)
+ *
+ * Use the settings below to configure session integration in your app.
+ * (for additional recommended settings, see `config/env/production.js`)
+ *
+ * For all available options, see:
+ * https://sailsjs.com/config/session
+ */
+
+module.exports.session = {
+
+  /***************************************************************************
+  *                                                                          *
+  * Session secret is automatically generated when your new app is created   *
+  * Replace at your own risk in production-- you will invalidate the cookies *
+  * of your users, forcing them to log in again.                             *
+  *                                                                          *
+  ***************************************************************************/
+  secret: '44bc2e3e7c74f0ddcbc6fe2305f07190',
+
+
+  /***************************************************************************
+  *                                                                          *
+  * Customize when built-in session support will be skipped.                 *
+  *                                                                          *
+  * (Useful for performance tuning; particularly to avoid wasting cycles on  *
+  * session management when responding to simple requests for static assets, *
+  * like images or stylesheets.)                                             *
+  *                                                                          *
+  * https://sailsjs.com/config/session                                       *
+  *                                                                          *
+  ***************************************************************************/
+  // isSessionDisabled: function (req){
+  //   return !!req.path.match(req._sails.LOOKS_LIKE_ASSET_RX);
+  // },
+
+};
